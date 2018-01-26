5acb6e139b41250bfe55728cbb462da57365fe99
Giulio Fagioli <fagioli.giulio@gmail.com>
2018-01-26 15:54:22 +0100

    Add classes for mobile menu

---
+++ dist/js/script.js
@@ -0,0 +1,18 @@
+let menuState = 0
+
+$("#mobile__button > i").on("click",()=>{
+  let mobileButton = $("#mobile__button > i")
+  if(menuState == 0){
+    $(".menu").addClass("menu__opened")
+    mobileButton.removeClass("fa-bars")
+    mobileButton.addClass("fa-times")
+    menuState = 1
+  }
+  else{
+    mobileButton.removeClass("fa-times")
+    mobileButton.addClass("fa-bars")
+    $(".menu").removeClass("menu__opened")
+    menuState = 0
+  }
+})
+//# sourceMappingURL=script.js.map
